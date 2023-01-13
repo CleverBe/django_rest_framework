9b7791d37d846516fea44970acf81e4c3263e94c
CleverBe <cleverbernal123@gmail.com>
2023-01-13 15:08:03 -0400

    arreglado db

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -83,12 +83,11 @@ WSGI_APPLICATION = 'drfsimplecrud.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    {
-        'default': dj_database_url.config(
-            default='sqlite:///db.sqlite3',
-            conn_max_age=600)
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600)
 }
+
 # 'default': {'ENGINE': 'django.db.backends.sqlite3','NAME': BASE_DIR / 'db.sqlite3',}
 
 # Password validation
